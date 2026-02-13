e7ce8fdc2f29b777e6f590187af32f7005bafd42
JoseMoran-ast <jose.moran@astrokube.com>
2026-02-13 10:52:30 +0100

    Add terraform and the CICD code for upload the model in EC2

---
+++ TF/main.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+}
+
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+# --- S3 Bucket ---
+resource "aws_s3_bucket" "mlops_bucket" {
+  bucket = var.bucket_name
+}
+
+
+# --- Network (default VPC) + Security Group ---
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Subnet pública por defecto (si hay varias, cogemos la primera)
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_security_group" "mlops_sg" {
+  name        = "mlops-sg"
+  description = "Security group for MLOps EC2"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "FastAPI"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = var.api_cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# --- EC2 ---
+resource "aws_instance" "mlops_ec2" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids      = [aws_security_group.mlops_sg.id]
+  associate_public_ip_address = true
+
+  iam_instance_profile = var.instance_profile_name
+
+  tags = {
+    Name = var.instance_name
+  }
+}
---
+++ TF/variables.tf
@@ -0,0 +1,59 @@
+variable "region" {
+  type    = string
+  default = "eu-north-1"
+}
+
+variable "access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "secret_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "Nombre único del bucket S3"
+}
+
+variable "instance_name" {
+  type    = string
+  default = "mlops-ec2"
+}
+
+variable "ami_id" {
+  type        = string
+  description = "AMI en eu-north-1 (Ubuntu/Amazon Linux)"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  type        = string
+  description = "Nombre del Key Pair existente en AWS"
+}
+
+# Para limitar SSH (recomendado: tu IP)
+variable "ssh_cidr_blocks" {
+  type        = list(string)
+  description = "CIDRs permitidos para SSH"
+  default     = ["0.0.0.0/0"]
+}
+
+# Para exponer la API (puedes limitarlo también)
+variable "api_cidr_blocks" {
+  type        = list(string)
+  description = "CIDRs permitidos para el puerto 8000"
+  default     = ["0.0.0.0/0"]
+}
+
+variable "instance_profile_name" {
+  type        = string
+  description = "Nombre del IAM Instance Profile (role) adjunto a la EC2"
+  default     = null
+}
